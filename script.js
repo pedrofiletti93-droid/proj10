3d603ebfad7ccbbc931c07af8e77c52b758b5c44
pedrofiletti93-droid <pedrofiletti93@gmail.com>
2026-05-19 11:38:01 -0300

    Pedro

---
+++ script.js
@@ -0,0 +1,13 @@
+const Aluno = [{name: "Pedro", idade: "17", disciplinapreferida: "Português"},
+                {name: "Givas", idade: "16", disciplinaprefida: "Matemática"},
+                {name: "Luan", idade: "16", disciplinapreferida: "Biologia"},
+                {name: "Vine",  idade: "16", disciplinapreferida: "Inglês"},
+                {name: "Wzin", idade: "16", disciplinapreferida: "Filosofia"}
+
+];
+
+Aluno.push({name: "Pedro", idade: "17", disciplinapreferida: "Português"});
+Aluno.push({name: "Wzin", idade: "16", disciplinapreferida: "Filosofia"});
+
+alert(`${Aluno[0].name}`);
+alert(`${Aluno[4].disciplinapreferida}`);
